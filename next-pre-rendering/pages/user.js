770f0050415057741826f0942e716d1aa18855e5
aurobindobhuyan <aurobindobhuyan6@gmail.com>
2022-08-31 10:10:30 +0530

    getStaticProps summary added in user page

--- next-pre-rendering/pages/user.js
+++ next-pre-rendering/pages/user.js
@@ -35,3 +35,23 @@ export async function getStaticProps() {
     },
   };
 }
+
+/*
+........getStaticProps contd.......
+1.
+- getStaticProps runs only on the server side.
+- The function will never run clinet-side.
+- The code you write inside getStaticProps won't even be included in the JS bundle that is sent to the browser.
+
+2.
+- You can write server-side code directly in getStaticProps.
+- Accessing the file system using the fs module or querying a database can be done inside getStaticProps.
+- You also don't have to worry about including API keys in getStaticProps as the won't make it to the browser.
+
+3.
+- getStaticProps is allowed onlyl in a page and cannot be run from regular component file. It is used for pre-rendering and not client-side data fetching.
+
+4. getStaticPops should return an object and object should contain a props key which is an object.
+
+5. getStaticProps will run at build time. However in developement mode when we run "npm run dev" getStaticProps runs on every request.
+*/
